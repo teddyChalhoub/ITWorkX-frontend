134167165bc0aebe81493bab08d430f7fb05cc6b
teddyChalhoub <teddychalhoub0@gmail.com>
2021-07-09 08:16:38 +0300

    updated the sub categories section to be responsive

--- src/components/category/category-component.js
+++ src/components/category/category-component.js
@@ -10,44 +10,45 @@ const SubCategories = (props) => {
 
   return (
     <div className="categories">
-      <p>Categories</p>
-
-      {props.categories &&
-        props.categories.map((category, index) => {
-          return (
-            <div
-              className="categories__left-border"
-              key={index}
-              onMouseEnter={() => {
-                setIsShown({
-                  visibility: "block",
-                });
-                setSubCategory(category.subCategory);
-              }}
-              onMouseLeave={() => {
-                setIsShown({ visibility: "none" });
-              }}
-            >
-              {category.category}
-            </div>
-          );
-        })}
-      {subCategory && ( 
-        <h3
-          onMouseEnter={() =>
-            setIsShown({
-              visibility: "block",
-            })
-          }
-          onMouseLeave={() => setIsShown({ visibility: "none" })}
-          style={{
-            height: `${props.categories.length * 2.638}rem`,
-            display: isShown.visibility,
-          }}
-          className=""
-        >
-          {subCategory}
-        </h3>
+      <div>
+        <p>Categories</p>
+        {props.categories &&
+          props.categories.map((category, index) => {
+            return (
+              <div
+                className="categories__left-border"
+                key={index}
+                onMouseEnter={() => {
+                  setIsShown({
+                    visibility: "block",
+                  });
+                  setSubCategory(category.subCategory);
+                }}
+                onMouseLeave={() => {
+                  setIsShown({ visibility: "none" });
+                }}
+              >
+                {category.category}
+              </div>
+            );
+          })}
+      </div>
+      {subCategory && (
+      <h3
+        className="subCategory__display"
+        onMouseEnter={() =>
+          setIsShown({
+            visibility: "block",
+          })
+        }
+        onMouseLeave={() => setIsShown({ visibility: "none" })}
+        style={{
+          height: `${props.categories.length * 2.638}rem`,
+          display: isShown.visibility,
+        }}
+      >
+        {subCategory}
+      </h3>
       )}
     </div>
   );
