bb3e9824e97ab76c74abf962518a8b02b264db90
Bachirm85 <mouawadbachir@gmail.com>
2021-07-12 12:44:39 +0300

    fixing the Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { BiUserCircle } from "react-icons/bi";
+//import { BiUserCircle } from "react-icons/bi";
 import Badge from "@material-ui/core/Badge";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import "./Navbar.css";
 
 function NavBar({ numberOfItemsInCart }) {
+  const [visible, setVisible] = useState(false);
+
+  const handleMouseEnter = (e) => {
+    e.preventDefault();
+    setVisible(true);
+  };
+
+  const handleMouseLeave = (e) => {
+    e.preventDefault();
+    setVisible(false);
+  };
+
   return (
     <div>
       <header>
         <div className="app__navbar">
           <div className="app__navbar__img">
-            <img className="logo" src="./img/LOGO.png" />
+            <img className="logo" src="./img/LOGO.png" alt="logo" />
             <input
               className="app__navbar__input"
               type="text"
@@ -35,18 +47,47 @@ function NavBar({ numberOfItemsInCart }) {
             <Link className="about-us links" to="/about-us">
               About us{" "}
             </Link>
-            <Badge
-              badgeContent={numberOfItemsInCart}
-              color="secondary"
-              className="cart__icon"
+
+            <Link to="/cart">
+              <Badge
+                badgeContent={numberOfItemsInCart} //TO ADD the number of items in cart
+                color="secondary"
+                className="cart__icon"
+              >
+                <ShoppingCartIcon style={{ fill: "white" }} fontSize="large" />
+              </Badge>
+            </Link>
+
+            <div
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              className="app__user__icon"
             >
-              <ShoppingCartIcon style={{ fill: "white" }} fontSize="large" />
-            </Badge>
-            <AccountCircleIcon
-              className="user__icon"
-              style={{ fill: "white" }}
-              fontSize="large"
-            />
+              <Link to="/picture">
+                <AccountCircleIcon
+                  className="user__icon"
+                  style={{ fill: "white" }}
+                  fontSize="large"
+                />
+              </Link>
+              <div className="app__user__log-in">
+                <Link
+                  className="app__log-in"
+                  style={{ display: visible ? "block" : "none" }}
+                  to="/log-in"
+                >
+                  Log In
+                </Link>
+
+                <Link
+                  className="app__sign-up"
+                  style={{ display: visible ? "block" : "none" }}
+                  to="/sign-up"
+                >
+                  Sign Up
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </header>
