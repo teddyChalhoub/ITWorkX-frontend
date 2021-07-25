37c0d417400f85ae6d91042e7c9c6536fa74a6e1
teddyChalhoub <teddychalhoub0@gmail.com>
2021-07-25 11:35:56 +0300

    added the secondary footer to the signin and signup

--- src/components/secondaryFooter/SecondaryFooter.js
+++ src/components/secondaryFooter/SecondaryFooter.js
@@ -1,18 +1,35 @@
-import React ,{useState}from 'react'
-import {FaFacebook,FaInstagram,FaTwitter,FaGooglePlusG} from 'react-icons/fa';
-import './SecondaryFooter.css'
+import React, { useState } from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaGooglePlusG,
+} from "react-icons/fa";
+import "./SecondaryFooter.css";
 
 export default function secondaryFooter() {
-     return (
+  return (
+    <div className="footer-content">
+      <div className="footer-content-message">
+        <h3>Get connected with us on socia media !</h3>
+      </div>
+      <div className="socials">
+        <a  className="fa-icon-color" href="https://www.facebook.com/">
+          <FaFacebook  />
+        </a>
 
-<div className="footer-content">
-            <h3>Get connected with us on socia media !</h3>
-            <ul className="socials">
-                <li><a className="fa-icon-color" href="https://www.facebook.com/"><FaFacebook/></a></li>
-                <li><a className="fa-icon-color" href="https://www.instagram.com/"><FaInstagram/></a></li>
-                <li><a className="fa-icon-color" href="https://www.twitter.com/"><FaTwitter/></a></li>
-                <li><a className="fa-icon-color" href="https://www.google.com/gmail/"><FaGooglePlusG/><a></a></a></li>
-            </ul>
-            </div>    
-     )
+        <a className="fa-icon-color" href="https://www.instagram.com/">
+          <FaInstagram />
+        </a>
+
+        <a className="fa-icon-color" href="https://www.twitter.com/">
+          <FaTwitter />
+        </a>
+
+        <a className="fa-icon-color" href="https://www.google.com/gmail/">
+          <FaGooglePlusG />
+        </a>
+      </div>
+    </div>
+  );
 }
