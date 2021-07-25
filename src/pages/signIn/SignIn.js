d2600ce125bafb66cc603e8e312a95c8cac61e80
teddyChalhoub <teddychalhoub0@gmail.com>
2021-07-25 12:51:07 +0300

    fixed signin design

--- src/pages/signIn/SignIn.js
+++ src/pages/signIn/SignIn.js
@@ -1,31 +1,43 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import './SignIn.css';
-import SecondaryFooter from '../../components/secondaryFooter/SecondaryFooter';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./SignIn.css";
+import SecondaryFooter from "../../components/secondaryFooter/SecondaryFooter";
 
-
-
-export default function SignUp() {
-    return (
-        
-        <div className="sign_in">
-            <form action="Sign_in_page">
-            <div className="mid-center">
+export default function SignIn() {
+  return (
+    <>
+      <div className="sign_in">
+        <form className="form-sign-in" action="Sign_in_page">
+          <div className="mid-center">
             <h1>Sign In </h1>
             <div className="sing_in_inputs">
-            <label for="email" className="label">Email</label><br/>
-            <input type="text" id="email" placeholder="Email" required /><br/>
-            <label for="password"className="label">Password</label><br/>
-            <input type="password" id="password" placeholder="Password" required /><br/>
+              <label for="email" className="label">
+                Email
+              </label>
+              <br />
+              <input type="text" id="email" placeholder="Email" required />
+              <br />
+              <label for="password" className="label">
+                Password
+              </label>
+              <br />
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                required
+              />
+              <br />
             </div>
-            <input type="button" class="button" value="Sign up"/>
-            <p>Not a member yet ?</p>  
-            <Link to="/signup"><p>Sign Up</p>
-            </Link> 
-            </div>
-            </form>
-            <SecondaryFooter/>
-
-        </div>
-    )
+            <input type="button" class="button" value="Sign in" />
+            <p>Not a member yet ?</p>
+            <Link to="/signup">
+              <p>Sign Up</p>
+            </Link>
+          </div>
+        </form>
+      </div>
+      <SecondaryFooter />
+    </>
+  );
 }
--- src/pages/signUp/SignUp.js
+++ src/pages/signUp/SignUp.js
@@ -1,49 +1,90 @@
-import React ,{useState}from 'react'
-import './SignUp.css';
-import SecondaryFooter from '../../components/secondaryFooter/SecondaryFooter';
-
+import React, { useState } from "react";
+import "./SignUp.css";
+import SecondaryFooter from "../../components/secondaryFooter/SecondaryFooter";
 
 export default function SignUp() {
+  const [wrong, setWrong] = useState(false);
+  const [password, setPassword] = useState("");
+  const [passwordAuth, setAuthPassword] = useState("");
 
-const [wrong, setWrong] = useState(false);
-const [password,setPassword] = useState("");
-const [passwordAuth,setAuthPassword] = useState("");
-
-const checkPassword = ()=>{
-    if(password !== passwordAuth){
-        setWrong(true);
+  const checkPassword = () => {
+    if (password !== passwordAuth) {
+      setWrong(true);
     }
-console.log("password",password);
-console.log("passwordAuth",passwordAuth);
-}
-    
-    return (
-        <div className="sign_up">
-           
-            <form onSubmit={(e)=>{e.preventDefault(); checkPassword()}} >
-            <p>Sign Up </p>
-            <div className="sing_in_inputs">
-            <input type="text" id="name" placeholder="Full name" required /><br/>
-            <input type="email" id="email" placeholder="Email" required /><br/>
-            <input className={wrong?"border-red":"border-normal"} type="password" value = {password} onChange={(e)=>{setPassword(e.target.value)}} id="password" placeholder={wrong?"Password doesn't match":"Password"} required /><br/>
-            <input className={wrong?"border-red":"border-normal"} type="password" value={passwordAuth}  onChange={(e)=>{setAuthPassword(e.target.value)}} id="passwordAuth" placeholder="Re-enter password" required /><br/>
-            <input type="text" id="Company" placeholder="Company name (optional)" /><br/>
-            <input type="number" id="employee" placeholder="Number of employee (optional)"/><br/>
-            <input type="country" id="Country" placeholder="Country" required /><br/>
-            <input type="text" id="Adrress" placeholder="Adrress" required  /><br/>
-            <div class="note">
-            <input type="text" id="notes" placeholder="Notes" />
-            </div>
-            <div className="signup_checkbox">
-            <label className="subs-check" for="Subs">Subscribe</label><br/>
-            <input type="checkbox" id="subs" name="subs" value="subs" />
-            </div>
-            </div>
-            <button  type="submit" className="button" >Sign Up </button>
-            </form>
-            <SecondaryFooter/>
+    console.log("password", password);
+    console.log("passwordAuth", passwordAuth);
+  };
 
-            
+  return (
+    <div className="sign_up">
+      <form
+        className="form-sign-up"
+        onSubmit={(e) => {
+          e.preventDefault();
+          checkPassword();
+        }}
+      >
+        <p>Sign Up </p>
+        <div className="sing_in_inputs">
+          <input type="text" id="name" placeholder="Full name" required />
+          <br />
+          <input type="email" id="email" placeholder="Email" required />
+          <br />
+          <input
+            className={wrong ? "border-red" : "border-normal"}
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            id="password"
+            placeholder={wrong ? "Password doesn't match" : "Password"}
+            required
+          />
+          <br />
+          <input
+            className={wrong ? "border-red" : "border-normal"}
+            type="password"
+            value={passwordAuth}
+            onChange={(e) => {
+              setAuthPassword(e.target.value);
+            }}
+            id="passwordAuth"
+            placeholder="Re-enter password"
+            required
+          />
+          <br />
+          <input
+            type="text"
+            id="Company"
+            placeholder="Company name (optional)"
+          />
+          <br />
+          <input
+            type="number"
+            id="employee"
+            placeholder="Number of employee (optional)"
+          />
+          <br />
+          <input type="country" id="Country" placeholder="Country" required />
+          <br />
+          <input type="text" id="Adrress" placeholder="Adrress" required />
+          <br />
+          <div class="note">
+            <input type="text" id="notes" placeholder="Notes" />
+          </div>
+          <div className="signup_checkbox">
+            <input type="checkbox" id="subs" name="subs" value="subs" />
+            <label className="subs-check" for="Subs">
+              Subscribe
+            </label>
+          </div>
         </div>
-    )
+        <button type="submit" className="button">
+          Sign Up{" "}
+        </button>
+      </form>
+      <SecondaryFooter />
+    </div>
+  );
 }
