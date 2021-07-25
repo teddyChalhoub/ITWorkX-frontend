e44f1e13992a0352f9865037e41d5114d1b8ffcb
teddyChalhoub <teddychalhoub0@gmail.com>
2021-07-25 21:47:22 +0300

    changed home content

--- src/components/hardwareSolution/HardwareSolution.js
+++ src/components/hardwareSolution/HardwareSolution.js
@@ -11,17 +11,9 @@ const HardwareSolution = () => {
       <div className="app__HardwareSolution--info">
         <h3 className="HardwareSolution">IT and Hardware Solution</h3>
         <p className="app__HardwareSolution__paragraph">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-          tristique porttitor rhoncus. Vestibulum in nibh bibendum, eleifend
-          risus quis, pellentesque leo. Curabitur scelerisque mollis lacinia.
-          Vestibulum ligula nibh, semper id vestibulum et, dapibus et odio. Sed
-          in sem et lectus luctus cursus nec in nunc. Suspendisse cursus erat
-          faucibus imperdiet suscipit. Pellentesque ut tortor ut justo
-          sollicitudin condimentum et vitae purus. Phasellus tristique porttitor
-          diam, sit amet facilisis sem sagittis at. Duis bibendum feugiat libero
-          lacinia convallis. Nullam sagittis massa sed dolor elementum
-          tristique. Suspendisse in metus libero. Aenean ullamcorper vestibulum
-          quam id sodales.
+          Are your searching for hardwares? Go check out our store where you can
+          find a wide range of hardwares, like CPU, Laptops parts,etc. Check out
+          our Store and find what you need.{" "}
         </p>
         <div className="app__btn__shop">
           <Link to="/store">
