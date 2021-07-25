c9db72402f3426ac55140226c3beffe88ae64e32
teddyChalhoub <teddychalhoub0@gmail.com>
2021-07-25 20:57:44 +0300

    finished the business and servies layout

--- src/components/AboutBusiness/AboutBusiness.js
+++ src/components/AboutBusiness/AboutBusiness.js
@@ -6,18 +6,9 @@ const AboutBusiness = () => {
     <div className="app__about-business">
       <h3 className="about-business">About the Business</h3>
       <p className="app__paragraph">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique
-        porttitor rhoncus. Vestibulum in nibh bibendum, eleifend risus quis,
-        pellentesque leo. Curabitur scelerisque mollis lacinia. Vestibulum
-        ligula nibh, semper id vestibulum et, dapibus et odio. Sed in sem et
-        lectus luctus cursus nec in nunc. Suspendisse cursus erat faucibus
-        imperdiet suscipit. Pellentesque ut tortor ut justo sollicitudin
-        condimentum et vitae purus. Phasellus tristique porttitor diam, sit amet
-        facilisis sem sagittis at. Duis bibendum feugiat libero lacinia
-        convallis. Nullam sagittis massa sed dolor elementum tristique.
-        Suspendisse in metus libero. Aenean ullamcorper vestibulum quam id
-        sodales.
+        ITWorkX concentrate on customer satisfaction. the main priority of this website is the ability to reach customer satisfaction and needs. We provide software development service and hardware parts.
       </p>
+      <p>"The sole reason we are in business is to make life less difficult for our clients." - Matthew Odgers</p>
     </div>
   );
 };
